9a054d6d3300ea5e645d5e2570b75574ef7821a8
Jacek Wronski <jacek.wronski.dev@gmail.com>
2020-03-27 15:11:35 +0100

    #7 - fix migrations

--- oxy-server/Migrations/02.sql
+++ oxy-server/Migrations/02.sql
@@ -2,8 +2,8 @@
 
 CREATE TABLE ppmConcentrations
 (
-    "Date" timestamp with time zone NOT NULL,
-    "Id" uuid NOT NULL,
-    "Concentration" integer NOT NULL,
-    CONSTRAINT "ppmConcentrations_pkey" PRIMARY KEY ("Id")
+    Date timestamp with time zone NOT NULL,
+    Id uuid NOT NULL,
+    Concentration integer NOT NULL,
+    CONSTRAINT ppmConcentrations_pkey PRIMARY KEY (Id)
 )
